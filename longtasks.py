f55c55f79af81fb265e0b2e138396f648a88f0cb
David <david@zmedianow.com>
2017-06-08 13:13:16 -0700

    threading for tasks

--- longtasks.py
+++ longtasks.py
@@ -1,6 +1,7 @@
 import os
 
 import logging
+import threading
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 
@@ -12,47 +13,62 @@ import datetime
 from django.utils import timezone
 from django.db.transaction import atomic
 import decimal
-import time
 
 import poloapi.restapi
 from rainmaker.models import LendHistory, LendStats
 from rainmaker.utils import BitcoinDecimal
 
+# freqency = 10  # seconds
+# seconds_in_2day = 86400 * 2
+# num_loops = (seconds_in_2day / freqency)
 
+
+@atomic
 def save_lending_stats():
-    freqency = 10  # seconds
-    seconds_in_2day = 86400 * 2
-    num_loops = (seconds_in_2day / freqency)
-    for x in xrange(num_loops):
-        logging.info('Save Called: {} loop'.format(x))
-        with atomic():
-            last_cycle = LendStats.objects.last()
-            if not last_cycle:
-                logging.debug('No Previous LendStats entry found.')
-            time_since_last = timezone.now() - last_cycle.created_at
-            if time_since_last < datetime.timedelta(seconds=9):
-                logging.warning('Save fired too quickly since last save. Time since last save {}'.found(
-                    time_since_last.seconds))
-                time.sleep(freqency)
-                continue
-            p = poloapi.restapi.poloniex()
-            objects_list = []
-            bid_asks = []
-            loans = p.returnLoanOrders('BTC')['offers']
-            i = 0
-            for loan in loans:
-                i += 1
-                percent = decimal.Decimal(loan['rate']) * 100
-                amount = BitcoinDecimal(loan['amount'])
-                objects_list.append(LendHistory(amount=amount,
-                                                interest_ask=percent))
-                if i > 25:
-                    bid_asks.append(percent)
-            LendHistory.objects.bulk_create(objects_list)
-            avg_low_bid = sum(bid_asks) / len(bid_asks)
-            LendStats.objects.create(avg_interest_ask=avg_low_bid)
-            logging.info('Completed: {} loop at price: {}'.format(x, avg_low_bid))
-        time.sleep(freqency)
+    last_cycle = LendStats.objects.last()
+    if not last_cycle:
+        logging.debug('No Previous LendStats entry found.')
+    time_since_last = timezone.now() - last_cycle.created_at
+    if time_since_last < datetime.timedelta(seconds=9):
+        logging.warning('Save fired too quickly since last save. Time since last save {}'.found(
+            time_since_last.seconds))
+        return
+    p = poloapi.restapi.poloniex()
+    objects_list = []
+    bid_asks = []
+    loans = p.returnLoanOrders('BTC')['offers']
+    i = 0
+    for loan in loans:
+        i += 1
+        percent = decimal.Decimal(loan['rate']) * 100
+        amount = BitcoinDecimal(loan['amount'])
+        objects_list.append(LendHistory(amount=amount,
+                                        interest_ask=percent))
+        if i > 25:
+            bid_asks.append(percent)
+    LendHistory.objects.bulk_create(objects_list)
+    avg_low_bid = sum(bid_asks) / len(bid_asks)
+    LendStats.objects.create(avg_interest_ask=avg_low_bid)
+    logging.info('Save completed at price: {}'.format(avg_low_bid))
 
 
-save_lending_stats()
+class perpetualTimer():
+    def __init__(self, t, hFunction):
+        self.i = 0
+        self.t = t
+        self.hFunction = hFunction
+        self.thread = threading.Timer(self.t, self.handle_function)
+
+    def handle_function(self):
+        self.hFunction()
+        if not self.i:
+            self.thread = threading.Timer(self.t, self.handle_function)
+            self.thread.start()
+        self.i += 1
+
+    def start(self):
+        self.thread.start()
+
+
+t = perpetualTimer(10.0, save_lending_stats)
+t.start()
