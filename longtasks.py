1664fe50fa1c4c6ba98ae58d6d56a22304f4fef5
David <david@zmedianow.com>
2017-06-08 11:38:06 -0700

    add frequency

--- longtasks.py
+++ longtasks.py
@@ -17,32 +17,37 @@ from rainmaker.models import LendHistory, LendStats
 from rainmaker.utils import BitcoinDecimal
 
 
-@atomic
 def save_lending_stats():
-    num_entries = LendStats.objects.count()
-    print 'CALLED DB STATS FUNCTION', num_entries
-    last_cycle = LendStats.objects.last()
-    if last_cycle and last_cycle.created_at + datetime.timedelta(seconds=8) > timezone.now():
-        print 'too early'
-        return
-    p = poloapi.restapi.poloniex()
-    objects_list = []
-    bid_asks = []
-    loans = p.returnLoanOrders('BTC')['offers']
-    i = 0
-    for loan in loans:
-        i += 1
-        percent = decimal.Decimal(loan['rate']) * 100
-        amount = BitcoinDecimal(loan['amount'])
-        objects_list.append(LendHistory(amount=amount,
-                                        interest_ask=percent))
-        if i > 25:
-            bid_asks.append(percent)
-    LendHistory.objects.bulk_create(objects_list)
-    avg_low_bid = sum(bid_asks) / len(bid_asks)
-    LendStats.objects.create(avg_interest_ask=avg_low_bid)
-    num_entries = LendStats.objects.count()
-    print 'cycle completed', LendStats.objects.last().avg_interest_ask, 'entries:', num_entries
-    time.sleep(10)
+    freqency = 10 #seconds
+    seconds_in_day = 86400
+    num_loops = (seconds_in_day / freqency)
+    print num_loops
+    for x in range(num_loops):
+        num_entries = LendStats.objects.count()
+        print 'CALLED DB STATS FUNCTION', num_entries
+        with atomic():
+            last_cycle = LendStats.objects.last()
+            if last_cycle and last_cycle.created_at + datetime.timedelta(seconds=8) > timezone.now():
+                print 'too early'
+                return
+            p = poloapi.restapi.poloniex()
+            objects_list = []
+            bid_asks = []
+            loans = p.returnLoanOrders('BTC')['offers']
+            i = 0
+            for loan in loans:
+                i += 1
+                percent = decimal.Decimal(loan['rate']) * 100
+                amount = BitcoinDecimal(loan['amount'])
+                objects_list.append(LendHistory(amount=amount,
+                                                interest_ask=percent))
+                if i > 25:
+                    bid_asks.append(percent)
+            LendHistory.objects.bulk_create(objects_list)
+            avg_low_bid = sum(bid_asks) / len(bid_asks)
+            LendStats.objects.create(avg_interest_ask=avg_low_bid)
+        num_entries = LendStats.objects.count()
+        print 'cycle completed', LendStats.objects.last().avg_interest_ask, 'entries:', num_entries
+        time.sleep(freqency)
 
 save_lending_stats()
