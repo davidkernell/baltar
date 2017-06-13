35c2a5f9a9a4b9a87dcca99e4d6193a9d8f35bc3
David <david@zmedianow.com>
2017-06-13 12:48:18 -0700

    management commands

--- poloapi/management/commands/poloapi_savelendingstats.py
+++ poloapi/management/commands/poloapi_savelendingstats.py
@@ -1,44 +1,45 @@
-import logging
-from django.utils import timezone
 import datetime
 import decimal
+import logging
 import time
+
+import django.core.management
+from django.utils import timezone
+
 import rainmaker.models
 from poloapi.restapi import poloniex
-freqency = 10  # seconds
-seconds_in_hour = 60 * 60
-num_loops = (seconds_in_hour / freqency)
 
 
-def save_lending_stats():
-    freqency = 10  # seconds
-    seconds_in_hour = 60 * 60
-    num_loops = (seconds_in_hour / freqency)
-    for x in xrange(num_loops):
-        last_cycle = rainmaker.models.LendStats.objects.last()
-        if not last_cycle:
-            logging.debug('No Previous LendStats entry found.')
-        time_since_last = time.timezone.now() - last_cycle.created_at
-        if time_since_last < datetime.timedelta(seconds=9):
-            logging.warning('Save fired too quickly since last save. Time since last save {}'.format(
-                time_since_last.seconds))
-            time.sleep(.5)
-        p = poloniex()
-        objects_list = []
-        bid_asks = []
-        loans = p.returnLoanOrders('BTC')['offers']
-        i = 0
-        for loan in loans:
-            i += 1
-            percent = decimal.Decimal(loan['rate']) * 100
-            amount = BitcoinDecimal(loan['amount'])
-            objects_list.append(rainmaker.models.LendHistory(amount=amount,
-                                                      interest_ask=percent))
-            if i > 25:
-                bid_asks.append(percent)
-        rainmaker.models.LendHistory.objects.bulk_create(objects_list)
-        avg_low_bid = sum(bid_asks) / len(bid_asks)
-        rainmaker.models.LendStats.objects.create(avg_interest_ask=avg_low_bid)
-        logging.info('Save completed at price: {}'.format(avg_low_bid))
-        print 'Save completed at price: {}'.format(avg_low_bid)
-        return
+class Command(django.core.management.BaseCommand):
+    def handle(self, *args, **options):
+        freqency = 10  # seconds
+        seconds_in_hour = 60 * 60
+        num_loops = (seconds_in_hour / freqency)
+        for x in xrange(num_loops):
+            last_cycle = rainmaker.models.LendStats.objects.last()
+            if not last_cycle:
+                logging.debug('No Previous LendStats entry found.')
+            time_since_last = timezone.now() - last_cycle.created_at
+            if time_since_last < datetime.timedelta(seconds=9):
+                logging.warning('Save fired too quickly since last save. Time since last save {}'.format(
+                    time_since_last.seconds))
+                time.sleep(.5)
+            p = poloniex()
+            objects_list = []
+            bid_asks = []
+            loans = p.returnLoanOrders('BTC')['offers']
+            i = 0
+            for loan in loans:
+                i += 1
+                percent = decimal.Decimal(loan['rate']) * 100
+                amount = decimal.Decimal(loan['amount'])
+                objects_list.append(rainmaker.models.LendHistory(amount=amount,
+                                                                 interest_ask=percent))
+                if i > 25:
+                    bid_asks.append(percent)
+            rainmaker.models.LendHistory.objects.bulk_create(objects_list)
+            avg_low_bid = sum(bid_asks) / len(bid_asks)
+            rainmaker.models.LendStats.objects.create(avg_interest_ask=avg_low_bid)
+            logging.info('Save completed at price: {}'.format(avg_low_bid))
+            print 'Save completed at price: {}'.format(avg_low_bid)
+            return
--- scraper/management/commands/scraper_safecoinform.py
+++ scraper/management/commands/scraper_safecoinform.py
@@ -1,6 +1,8 @@
 import json
 
 import logging
+
+import django.core.management
 import requests
 import twilio.rest
 
@@ -10,48 +12,50 @@ import scraper.models
 from django.conf import settings
 
 
-def check_for_safe_dev_post():
-    FORM_URL = 'http://forum.safedev.org/'
-    DEV_ACCT_ID = 205
+class Command(django.core.management.BaseCommand):
+    def handle(self, *args, **options):
+        FORM_URL = 'http://forum.safedev.org/'
+        DEV_ACCT_ID = 205
 
-    known_posts = scraper.models.MaidSafeFormPost.objects.values_list('topic_id', flat=True)
-    page = requests.get(FORM_URL)
-    # dirty as hell
-    start_str = 'ps.store("topic_list_latest"'
-    end_str = '}]}]}});\n'
-    data_start = page.content.find(start_str) + len(start_str) + 2
-    data_end = page.content.find(end_str, data_start) + len(end_str) - 3
-    post_data = json.loads(page.content[data_start:data_end])
-    for topic in post_data['topic_list']['topics']:
-        if topic['id'] in known_posts:
-            continue
-        for posters in topic['posters']:
-            if posters[u'description'] == u'Original Poster':
-                scraper.models.MaidSafeFormPost.objects.create(topic_id=topic['id'],
-                                                               dev_blog=True if posters[
-                                                                                    'user_id'] == DEV_ACCT_ID else False)
-                if posters['user_id'] == DEV_ACCT_ID:
-                    client = twilio.rest.Client(settings.ACCOUNT_SID, settings.AUTH_TOKEN)
+        known_posts = scraper.models.MaidSafeFormPost.objects.values_list('topic_id', flat=True)
+        page = requests.get(FORM_URL)
+        # dirty as hell
+        start_str = 'ps.store("topic_list_latest"'
+        end_str = '}]}]}});\n'
+        data_start = page.content.find(start_str) + len(start_str) + 2
+        data_end = page.content.find(end_str, data_start) + len(end_str) - 3
+        post_data = json.loads(page.content[data_start:data_end])
+        for topic in post_data['topic_list']['topics']:
+            if topic['id'] in known_posts:
+                continue
+            for posters in topic['posters']:
+                if posters[u'description'] == u'Original Poster':
+                    scraper.models.MaidSafeFormPost.objects.create(topic_id=topic['id'],
+                                                                   dev_blog=True if posters[
+                                                                                        'user_id'] == DEV_ACCT_ID else False)
+                    if posters['user_id'] == DEV_ACCT_ID:
+                        client = twilio.rest.Client(settings.ACCOUNT_SID, settings.AUTH_TOKEN)
 
-                    message = client.messages.create(settings.ADMIN_PHONE,
-                        from_=settings.TWILIO_PHONE,
-                        body="NEW MAIDSAFE DEV POST!\n{}".format(FORM_URL))
-                    print(message.sid)
-                    logging.warning('New Dev Post Found!')
-                    try:
-                        coin = dynamic_settings.models.Coin.objects.get(ticker='MAID')
-                    except:
-                        logging.warning('MaidSafeCoin not found in Coin DB')
-                    try:
-                        coin_settings = dynamic_settings.models.DynamicTradeSetting.objects.get(coin=coin)
-                    except:
-                        logging.warning('MaidSafeCoin dynamic settings found in Coin DB')
-                        continue
-                    if coin_settings.can_trade:
-                        logging.warning('attempting margin buy on safecoin for the amount of {} btc'.format(settings.SAFECOIN_DEVPOST_TRADE))
+                        message = client.messages.create(settings.ADMIN_PHONE,
+                                                         from_=settings.TWILIO_PHONE,
+                                                         body="NEW MAIDSAFE DEV POST!\n{}".format(FORM_URL))
+                        print(message.sid)
+                        logging.warning('New Dev Post Found!')
+                        try:
+                            coin = dynamic_settings.models.Coin.objects.get(ticker='MAID')
+                        except:
+                            logging.warning('MaidSafeCoin not found in Coin DB')
+                        try:
+                            coin_settings = dynamic_settings.models.DynamicTradeSetting.objects.get(coin=coin)
+                        except:
+                            logging.warning('MaidSafeCoin dynamic settings found in Coin DB')
+                            continue
+                        if coin_settings.can_trade:
+                            logging.warning('attempting margin buy on safecoin for the amount of {} btc'.format(
+                                settings.SAFECOIN_DEVPOST_TRADE))
 
-                    else:
-                        logging.warning('MaidSafeCoin dynamic trading turned off')
-                        continue
+                        else:
+                            logging.warning('MaidSafeCoin dynamic trading turned off')
+                            continue
 
-    logging.info('SafeCoin form scraped, no new dev posts')
+        logging.info('SafeCoin form scraped, no new dev posts')
